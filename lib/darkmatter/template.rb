a2bc7c93e5a0292eac5dbc737bfbd7bde07d18fc
Blake Taylor <blakefrost@gmail.com>
2013-06-18 21:13:38 -0700

    Only use one data hash, use hash with indifferent access
    Value of @page was being overridden when rendering partials. This change causes
    all yaml front matter to be merged in the order it is rendered.
    
    Also uses hash with indifferent access for convenience with hash access.

--- lib/darkmatter/template.rb
+++ lib/darkmatter/template.rb
@@ -2,13 +2,17 @@ module ActionView
   class Template
     alias_method :_old_initialize, :initialize
     def initialize(source, identifier, handler, details)
-      @data, source = parse_yaml_front_matter(source)
+      _data, source = parse_yaml_front_matter(source)
+      @data ||= HashWithIndifferentAccess.new
+      @data.merge!(_data)
       _old_initialize(source, identifier, handler, details)
     end
 
     alias_method :_old_render, :render
     def render(view, locals, buffer=nil, &block)
-      view.instance_variable_set(:@page, @data)
+      unless view.instance_variable_get(:@page)
+        view.instance_variable_set(:@page, @data)
+      end
       _old_render(view, locals, buffer, &block)
     end
 
